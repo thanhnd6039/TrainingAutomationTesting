bd8206f5ca3d85abcadaf02fb54d8ff3aa6e25f7
thanhnd6039 <thanhvirtium@gmail.com>
2020-05-27 07:26:55 +0700

    continue

--- Desktop/src/test/java/cucumber/SeleniumTest.java
+++ Desktop/src/test/java/cucumber/SeleniumTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -12,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class SeleniumTest {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
     public static void main(String[] args) throws InterruptedException {
         String projectDirPath =  System.getProperty("user.dir");
         String driverPath = projectDirPath + "\\src\\test\\resources\\driver\\windows\\chromedriver.exe";
@@ -19,6 +22,7 @@ public class SeleniumTest {
         String searchStr = "?s=dress&post_type=product";
         System.setProperty("webdriver.chrome.driver",driverPath);
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,15);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(baseUrl);
@@ -31,6 +35,9 @@ public class SeleniumTest {
         WebElement sizeOption = driver.findElement(By.id("pa_size"));
         Select size = new Select(sizeOption);
         size.selectByValue("medium");
+        WebElement btnAddToCart = driver.findElement(By.xpath("//button[@class='single_add_to_cart_button button alt']"));
+        wait.until(ExpectedConditions.elementToBeClickable(btnAddToCart));
+        
         Thread.sleep(3000);
         driver.quit();
     }
