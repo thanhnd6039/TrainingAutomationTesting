da336f5c6da53f0e8f8142937f9462a4b7f3b95f
thanhnd6039 <thanhvirtium@gmail.com>
2020-05-30 14:25:28 +0700

    Commit to branch

--- Desktop/src/test/java/cucumber/SeleniumTest.java
+++ Desktop/src/test/java/cucumber/SeleniumTest.java
@@ -5,10 +5,6 @@ import cucumber.data_providers.ConfigFileReader;
 public class SeleniumTest {
 
     public static void main(String[] args){
-        ConfigFileReader configFileReader = new ConfigFileReader();
-        String driverPath = configFileReader.getDriverPath();
-        System.out.println("driverPath: "+driverPath);
-        String url = configFileReader.geturl();
-        System.out.println("url: "+url);
+        
     }
 }
